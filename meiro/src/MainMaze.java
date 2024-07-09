81852e31c38059f74fd868814a6a2bb3ffb2aebf
rui <rui101304@gmail.com>
2024-07-09 09:44:55 +0900

    maze1

---
+++ meiro/src/MainMaze.java
@@ -0,0 +1,9 @@
+
+public class MainMaze {
+	public class main extends MyFrame {
+		public void run() {
+			Maze maze = new Maze(15, 15);
+			maze.show();
+		}
+	}
+}
---
+++ meiro/src/MazeFrame.java
@@ -0,0 +1,7 @@
+import java.awt.Frame;
+
+public class MazeFrame extends Frame {
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+}
